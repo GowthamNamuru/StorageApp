1df79bd5719caff573f1200db0d2834da38fcc0d
gowthamnamuru <gowthamnamuri@gmail.com>
2024-04-14 10:16:40 +0530

    Initial Commit

---
+++ StorageApp/StorageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageAppApp.swift
+//  StorageApp
+//
+//  Created by Gowtham Namuru on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct StorageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
