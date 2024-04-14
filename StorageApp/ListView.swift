b94a86fef69f91b34acd908adab9a0fdb00da5c3
gowthamnamuru <gowthamnamuri@gmail.com>
2024-04-14 20:51:22 +0530

    Added ListView

---
+++ StorageApp/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  StorageApp
+//
+//  Created by Gowtham Namuru on 14/04/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
